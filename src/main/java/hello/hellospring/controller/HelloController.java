2f14f918a471d0336cdce6efd0f6234403fd5c42
jangdayeon <hyejmh@naver.com>
2023-07-30 13:35:05 +0900

    02:API

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -21,5 +22,33 @@ public class HelloController {
     public String helloMvc(@RequestParam(value = "name") String name, Model model){
         model.addAttribute("name",name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // HttpMessageConverter에게 넘김
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name; //이건 html의 body에 return된 코드가 그대로 들어가 출력됨
+        // String으로 그냥 넘김
+    }
+
+    @GetMapping("hello-api") //JSON방식으로 출력됨
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+        //객체니깐 Json으로 넘김
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
